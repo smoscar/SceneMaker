c95da862e317209b00070bbe2b10fcd6ca007414
Oscar <oarias@jondo-usa.com>
2017-06-26 17:01:06 -0700

    Feature to duplicate entire cells

--- js/SnapSVGAnimator/SnapSVGAnimator.js
+++ js/SnapSVGAnimator/SnapSVGAnimator.js
@@ -1520,7 +1520,7 @@ var ResourceManager = function (data) {
         }
     }
 
-    console.log(this.m_data.DOMDocument);
+    //console.log(this.m_data.DOMDocument);
 };
 
 //Member functions
@@ -1559,7 +1559,7 @@ function SVGAnim(data, w, h, fps, params) {
     instance.version = '1.2.3';
 
     msg = 'Snap.svg Animator v' + instance.version;
-    console.log("%c" + msg, "color:" + color + ";font-weight:bold;");
+    //console.log("%c" + msg, "color:" + color + ";font-weight:bold;");
 
     params = params || {};
     fps = fps || 24;
